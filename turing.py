0aaae387885a495b7f80b2c295480d667e2a8210
Zabari <zabari.daniel@gmail.com>
2019-03-20 02:17:21 -0400

    added r/w head simulatien

--- turing.py
+++ turing.py
@@ -17,34 +17,59 @@ def getch():
     return ch
 
 
-CURRENT_STATE = None
+current_state = None
 
-STATE_TABLE = collections.defaultdict(lambda: False)
+state_table = collections.defaultdict(lambda: False)
 
-TAPE = ""
+tape = ""
+head_index = 0
 
 with open("program.txt") as file_:
     for line in file_:
         if (line[:2] != "//"):
             temp = line.split()
-            if (CURRENT_STATE is None):
-                CURRENT_STATE = temp[0]
-            if (temp[0] not in STATE_TABLE):
-                STATE_TABLE[temp[0]] = collections.defaultdict(lambda: False)
-            STATE_TABLE[temp[0]][temp[1]] = tuple(temp[2:5])
+            if (current_state is None):
+                current_state = temp[0]
+            if (temp[0] not in state_table):
+                state_table[temp[0]] = collections.defaultdict(lambda: False)
+            state_table[temp[0]][temp[1]] = tuple(temp[2:5])
 
+# character = getch()
 while True:
-    character = getch()
+    # print(head_index, len(tape))
+    if (head_index < 0):
+        print("Fatal error. Terminating.")
+        break
+    if (head_index == len(tape)):
+        character = getch()
+    else:
+        character = tape[head_index]
+    # print(character)
     # Close on ctrl-c.
     if (ord(character) == 3):
         break
-    if (STATE_TABLE[CURRENT_STATE][character]):
-        TAPE += STATE_TABLE[CURRENT_STATE][character][1]
-        CURRENT_STATE = STATE_TABLE[CURRENT_STATE][character][0]
-        if (CURRENT_STATE == "f"):
+    if (state_table[current_state][character]):
+        if (head_index == len(tape)):
+            tape += state_table[current_state][character][1]
+        else:
+            tape = (tape[:head_index] +
+                    state_table[current_state][character][1] +
+                    tape[head_index + 1:]
+                    )
+        direction = state_table[current_state][character][2]
+        current_state = state_table[current_state][character][0]
+        if (current_state == "f"):
+            print(tape)
             print("Accepted. Terminating.")
             break
+        if (direction == "R"):
+            head_index += 1
+        elif (direction == "L"):
+            head_index -= 1
+        else:
+            print("Fatal error. Terminating.")
+            break
     else:
-        print("Fatal error. Turing Machine crashing.")
+        print("Fatal error. Terminating.")
         break
-    print(TAPE)
+    print(tape)
