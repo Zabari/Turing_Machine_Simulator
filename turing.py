8c9be138c40d85cab645eb2153e06bfb39bf6ac7
Zabari <zabari.daniel@gmail.com>
2019-04-09 16:25:50 -0400

    Made some style changes

--- turing.py
+++ turing.py
@@ -3,6 +3,7 @@ import tty
 import sys
 import termios
 import time
+import os
 
 
 # Taken from https://stackoverflow.com/a/510364/3217409.
@@ -16,16 +17,20 @@ def getch():
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
+# End copy paste
 
 
 current_state = None
-
 state_table = collections.defaultdict(lambda: False)
-
 tape = ""
 head_index = 0
+file_path = input("Please enter program file path: ")
 
-with open("program.txt") as file_:
+while (not os.path.isfile(file_path)):
+    print("That file does not exist.")
+    file_path = input("Please enter program file path: ")
+
+with open(file_path) as file_:
     for line in file_:
         if (line[:2] != "//"):
             temp = line.split()
@@ -35,28 +40,37 @@ with open("program.txt") as file_:
                 state_table[temp[0]] = collections.defaultdict(lambda: False)
             state_table[temp[0]][temp[1]] = tuple(temp[2:5])
 
-# character = getch()
-print("Program is running. Head is signified by @.")
+print("Program is running. Head is signified by (q<state number>).")
 while True:
+    # Sleeps for aesthetics.
     time.sleep(.1)
-    # print(head_index, len(tape))
-    print("Tape is currently:\n" + tape[:head_index] + "@" + tape[head_index:])
+    print(
+        "Tape is currently:\n" +
+        tape[:head_index] +
+        f"(q{current_state})" + tape[head_index:]
+    )
+    # Tape is only infinite in one direction, so if head is trying to move
+    # past the left edge.
     if (head_index < 0):
         print("Fatal error. Terminating.")
         break
+    # If head is at the right end of the tape.
     if (head_index == len(tape)):
         print("Waiting for input. Use B for blank.")
         character = getch()
+    # If the head is somewhere else.
     else:
         character = tape[head_index]
-    # print(character)
     # Close on ctrl-c.
     if (ord(character) == 3):
         break
     print("Tape head is reading:", character)
+    # If the state has a transition for the given input.
     if (state_table[current_state][character]):
+        # If at right edge, just add to tape.
         if (head_index == len(tape)):
             tape += state_table[current_state][character][1]
+        # Otherwise, write in place.
         else:
             tape = (tape[:head_index] +
                     state_table[current_state][character][1] +
@@ -68,16 +82,20 @@ while True:
             head_index += 1
         elif (direction == "L"):
             head_index -= 1
+        # If there is somehow a mistake in the direction.
         else:
             print("Fatal error. Terminating.")
             break
+        # At final state, print tape and exit.
         if (current_state == "f"):
-            # print(tape)
             print("Final state reads:")
-            print(tape[:head_index] + "@" + tape[head_index:])
+            print(
+                tape[:head_index] +
+                f"(q{current_state})" + tape[head_index:]
+            )
             print("Accepted. Terminating.")
             break
+    # If no transition for given input exists at state.
     else:
         print("Fatal error. Terminating.")
         break
-    # print(tape)
